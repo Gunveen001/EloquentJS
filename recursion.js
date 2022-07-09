5601d400afcbab1f8a8bdfd8d9882ec192587ef5
Gunveen Kour <gunveenkour61@gmail.com>
2022-07-09 17:39:25 +0530

    chapter 3 exercise 2

---
+++ recursion.js
@@ -0,0 +1,9 @@
+function isEven(n){
+    if(n === 0) return true;
+    if(n === 1) return false;
+    if(n<0) return isEven(-n);
+    return isEven(n-2);
+}
+console.log(isEven(20));
+console.log(isEven(35));
+console.log(isEven(-1));
